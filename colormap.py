d198a08c73d256a6d8beeb6a07fc7ba31faa481b
Tobias Petzak <Tobias Petzak>
2016-08-13 02:56:36 +0200

    colormap.py optimiert für schnellere berechnung. Es muss nicht noch noch einmal durch die gesamte Matrix itteriert werden, es wird direkt beim berechnen der kleinste Wert ausgerechnet

--- colormap.py
+++ colormap.py
@@ -7,14 +7,13 @@ def plot_colormap(matrix):
     #matrix = np.array([[0.000, -0.120, -0.043, -0.094, 0.037, 0.045], [0.120, 0.000, 0.108, 0.107, 0.105, 0.108], [0.043, 0.108, 0.000, 0.083, -0.043, 0.042], [0.094, 0.107, 0.083, 0.000, -0.083, 0.089], [-0.037, 0.105, -0.043, -0.083, 0.000, 2.440], [-0.045, -0.108, -0.042, 0.089, 2.440, 0.000]])
     #print(matrix)
     #cmap = [('Sequential (2)', 'afmhot')]
-    minmatrix=0.
-    minhelp=0.
+    
     #find min of array
-    for i in range (1,len(matrix)):
-        for j in range (1,len(matrix[0])):
-            minhelp=matrix[i,j]
-            if minhelp<minmatrix:
-                minmatrix=minhelp
+    #for i in range (1,len(matrix)):
+     #   for j in range (1,len(matrix[0])):
+      #      minhelp=matrix[i,j]
+           # if minhelp<minmatrix:
+            #    minmatrix=minhelp
 
     #print(minmatrix)
     matrix = np.ma.masked_where(matrix>0,matrix)
@@ -32,15 +31,17 @@ def plot_colormap(matrix):
 #ax.invert_yaxis()
 #plt.show()
 
-data = np.zeros((1024,1024))
-a= 1.
-b= 1.7
+minmatrix=0.
+pixels = 1024
+data = np.zeros((pixels,pixels))
+a= 1.9
+b= 3.4
 total=0.
-x = 0.145 #start_value
+x = 0.0145 #start_value
 step=(2)/1024
-for k in range(1,1024):
+for k in range(1,pixels):
     a=a+(k-1)*step
-    for j in range(1,1024):
+    for j in range(1,pixels):
         b=b+(j-1)*step
         for n in range(1,2000):
             for i in range(1,2):
@@ -54,6 +55,8 @@ for k in range(1,1024):
                     x = r*x*(1-x)
                     total= total+(math.log(abs(r-(2*r*x)))/math.log(2))
         lyap=total/4000
+        if(lyap<minmatrix):
+                    minmatrix=lyap
         data[k][j]=lyap
 
 plot_colormap(data)    
