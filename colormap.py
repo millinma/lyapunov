03d3e2b08da446d6eb0a801e78f3348323eea250
millinma <milling.manuel@gmail.com>
2016-08-24 13:18:04 +0200

    added independent parameter ranges for r in a and b

--- colormap.py
+++ colormap.py
@@ -5,40 +5,46 @@ import math
 #def plot_colormap(matrix):
     
     #matrix = np.array([[0.000, -0.120, -0.043, -0.094, 0.037, 0.045], [0.120, 0.000, 0.108, 0.107, 0.105, 0.108], [0.043, 0.108, 0.000, 0.083, -0.043, 0.042], [0.094, 0.107, 0.083, 0.000, -0.083, 0.089], [-0.037, 0.105, -0.043, -0.083, 0.000, 2.440], [-0.045, -0.108, -0.042, 0.089, 2.440, 0.000]])
-   
+
 
 minmatrix=0.
-pixels = 100 #Aufloesung des Bildes
+pixelsa = 75 #Aufloesung des Bildes
+pixelsb = 100
+preiterations = 600
 lyap_iterations = 2000
-matrix = np.zeros((pixels,pixels))   #erzeugt matrix mit der gewaehlten Aufloesung. Datenwert entspricht lyapunv exponent
-sequence = "ab"
-min = 3
-max = 4.
+matrix = np.zeros((pixelsa,pixelsb))   #erzeugt matrix mit der gewaehlten Aufloesung. Datenwert entspricht lyapunv exponent
+sequence = "bbaba"
+mina = 3.6
+maxa = 3.9
+minb = 3.2
+maxb = 3.6
 x = 0.23 #start_value
-step=(max-min)/pixels
-for k in range(pixels): #iteriert durch die Zeilen der matrix
-    a=min + k*step
+stepa = (maxa-mina)/pixelsa
+stepb = (maxb-minb)/pixelsb
+for k in range(pixelsa): #iteriert durch die Zeilen der matrix
+    a = mina + k*stepa
     print(k)
-    for j in range(pixels): #iteriert durch die spalten der matrix
-        b=min + j*step
+    for j in range(pixelsb): #iteriert durch die spalten der matrix
+        b = minb + j*stepb
         total = 0.
-        for _ in range(lyap_iterations):   #hier lauft der pseudocode aus dem paper durch
+        for n in range((lyap_iterations + preiterations)/len(sequence)):   #hier lauft der pseudocode aus dem paper durch
             for r_str in sequence:
                 if(r_str == "a"):
                     r=a
                 elif(r_str == "b"):
                     r=b
                 x = r*x*(1-x)
-                total= total+(math.log(abs(r-(2*r*x)))/math.log(2))
+                if(n > preiterations/len(sequence)):
+                    total= total+(math.log(abs(r-(2*r*x)))/math.log(2))
         lyap=total / (lyap_iterations * len(sequence))
         if(lyap<minmatrix):
             minmatrix=lyap
-        matrix[pixels - 1 - k][j]=lyap   #ergebnis wird der matrix zugewiesen
+        matrix[pixelsa - 1 - k][j]=lyap   #ergebnis wird der matrix zugewiesen
 #plot_colormap(data)
 matrix = np.ma.masked_where(matrix>0,matrix)
 cmap = plt.cm.YlOrBr
 cmap.set_bad(color='black')
-plt.imshow(matrix, interpolation='none', cmap=cmap, extent = [min, max, min, max])
-filename = "./pictures/p" + str(pixels) +  "_" + sequence + "_" + str(min) + "-" + str(max) + ".png"
+plt.imshow(matrix, interpolation='none', cmap=cmap, extent = [mina, maxa, minb, maxb])
+filename = "./pictures/p" + str(pixelsa) +  "_" + sequence + "_" + str(mina) + "-" + str(maxa) + ".png"
 plt.savefig(filename)
 plt.show()
