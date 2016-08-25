69b1f08b9d78c96571f89672e2d09990206e32e8
millinma <milling.manuel@gmail.com>
2016-08-25 15:26:12 +0200

    small fixes in colormap

--- colormap.py
+++ colormap.py
@@ -2,22 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-#def plot_colormap(matrix):
-    
-    #matrix = np.array([[0.000, -0.120, -0.043, -0.094, 0.037, 0.045], [0.120, 0.000, 0.108, 0.107, 0.105, 0.108], [0.043, 0.108, 0.000, 0.083, -0.043, 0.042], [0.094, 0.107, 0.083, 0.000, -0.083, 0.089], [-0.037, 0.105, -0.043, -0.083, 0.000, 2.440], [-0.045, -0.108, -0.042, 0.089, 2.440, 0.000]])
-
 
 minmatrix=0.
-pixelsa = 75 #Aufloesung des Bildes
+pixelsa = 100 #Aufloesung des Bildes
 pixelsb = 100
 preiterations = 600
-lyap_iterations = 2000
+lyap_iterations = 4000
 matrix = np.zeros((pixelsa,pixelsb))   #erzeugt matrix mit der gewaehlten Aufloesung. Datenwert entspricht lyapunv exponent
-sequence = "bbaba"
-mina = 3.6
+sequence = "bbbbbbaaaaaa"
+mina = 3.7
 maxa = 3.9
-minb = 3.2
-maxb = 3.6
+minb = 3.0
+maxb = 3.3
 x = 0.23 #start_value
 stepa = (maxa-mina)/pixelsa
 stepb = (maxb-minb)/pixelsb
@@ -44,6 +40,7 @@ for k in range(pixelsa): #iteriert durch die Zeilen der matrix
 matrix = np.ma.masked_where(matrix>0,matrix)
 cmap = plt.cm.YlOrBr
 cmap.set_bad(color='black')
+plt.axis("equal")
 plt.imshow(matrix, interpolation='none', cmap=cmap, extent = [mina, maxa, minb, maxb])
 filename = "./pictures/p" + str(pixelsa) +  "_" + sequence + "_" + str(mina) + "-" + str(maxa) + ".png"
 plt.savefig(filename)
